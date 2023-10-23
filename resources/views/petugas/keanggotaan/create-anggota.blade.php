e786c1f0ebc763b4198f7b238d15034f7e316ad1
Andika Auli <dikaauli25@gmail.com>
2023-10-23 12:08:17 +0700

    [FIX] create anggota

--- resources/views/petugas/keanggotaan/create-anggota.blade.php
+++ resources/views/petugas/keanggotaan/create-anggota.blade.php
@@ -17,19 +17,6 @@
     <form action="{{ route('client.create-member') }}" method="POST" class="m-0 p-0">
         @csrf
         <div class="bg-white">
-         {{-- ID Anggota --}}
-             <div class="flex border-y border-solid border-gray-300">
-                 <div class="px-4 py-3 text-sm w-60">
-                     <p class="font-bold text-sm">ID Anggota*</p>
-                 </div>
-                 <div class="px-4 py-3 text-sm">
-                     <p class="font-bold text-sm">:</p>
-                 </div>
-                 <div class="px-4 py-3">
-                     <input type="text" id="small-input" class="w-96 py-1 px-2 text-gray-900 border rounded text-sm border-solid border-gray-400 focus:ring focus:ring-blue-300">
-                 </div>
-             </div>
-         {{-- End ID Anggota --}}
          {{-- Nama Anggota --}}
              <div class="flex border-b border-solid border-gray-300">
                  <div class="px-4 py-3 text-sm w-60">
@@ -39,10 +26,23 @@
                      <p class="font-bold text-sm">:</p>
                  </div>
                  <div class="px-4 py-3 items-stretch flex-auto">
-                     <input type="text" id="small-input" class="w-96 py-1 px-2 text-gray-900 border rounded text-sm border-solid border-gray-400 focus:ring focus:ring-blue-300">
+                     <input type="text" name="name" id="small-input" class="w-96 py-1 px-2 text-gray-900 border rounded text-sm border-solid border-gray-400 focus:ring focus:ring-blue-300">
                  </div>
              </div>
          {{-- End Nama Anggota --}}
+         {{-- NIM Anggota --}}
+            <div class="flex border-y border-solid border-gray-300">
+                <div class="px-4 py-3 text-sm w-60">
+                    <p class="font-bold text-sm">NIM Anggota*</p>
+                </div>
+                <div class="px-4 py-3 text-sm">
+                    <p class="font-bold text-sm">:</p>
+                </div>
+                <div class="px-4 py-3">
+                    <input type="text" name="nim" id="small-input" class="w-96 py-1 px-2 text-gray-900 border rounded text-sm border-solid border-gray-400 focus:ring focus:ring-blue-300">
+                </div>
+            </div>
+        {{-- End NIM Anggota --}}
          {{-- Tanggal Lahir --}}
              <div class="flex border-b border-solid border-gray-300">
                  <div class="px-4 py-3 text-sm w-60">
@@ -52,49 +52,10 @@
                      <p class="font-bold text-sm">:</p>
                  </div>
                  <div class="px-4 py-3">
-                     <input class="px-2 py-1.5 text-gray-900 border rounded text-sm border-solid border-gray-400 focus:ring focus:ring-blue-300" type="date" name="dateofbirth" id="dateofbirth">
+                     <input  class="px-2 py-1.5 text-gray-900 border rounded text-sm border-solid border-gray-400 focus:ring focus:ring-blue-300" type="date" name="birth_date" id="dateofbirth">
                  </div>
              </div>
          {{-- End Tanggal Lahir --}}
-         {{-- Anggota Sejak --}}
-             <div class="flex border-b border-solid border-gray-300">
-                 <div class="px-4 py-3 text-sm w-60">
-                     <p class="font-bold text-sm">Anggota Sejak</p>
-                 </div>
-                 <div class="px-4 py-3 text-sm">
-                     <p class="font-bold text-sm">:</p>
-                 </div>
-                 <div class="px-4 py-3">
-                     <input class="px-2 py-1.5 text-gray-900 border rounded text-sm border-solid border-gray-400 focus:ring focus:ring-blue-300" type="date" name="dateofbirth" id="dateofbirth">
-                 </div>
-             </div>
-         {{-- End Anggota Sejak --}}
-         {{-- Tanggal Registrasi --}}
-             <div class="flex border-b border-solid border-gray-300">
-                 <div class="px-4 py-3 text-sm w-60">
-                     <p class="font-bold text-sm">Tanggal Registrasi</p>
-                 </div>
-                 <div class="px-4 py-3 text-sm">
-                     <p class="font-bold text-sm">:</p>
-                 </div>
-                 <div class="px-4 py-3">
-                     <input class="px-2 py-1.5 text-gray-900 border rounded text-sm border-solid border-gray-400 focus:ring focus:ring-blue-300" type="date" name="dateofbirth" id="dateofbirth">
-                 </div>
-             </div>
-         {{-- End Tanggal Registrasi --}}
-         {{-- Berlaku Hingga --}}
-         <div class="flex border-b border-solid border-gray-300">
-             <div class="px-4 py-3 text-sm w-60">
-                 <p class="font-bold text-sm">Berlaku Hingga</p>
-             </div>
-             <div class="px-4 py-3 text-sm">
-                 <p class="font-bold text-sm">:</p>
-             </div>
-             <div class="px-4 py-3">
-                 <input class="px-2 py-1.5 text-gray-900 border rounded text-sm border-solid border-gray-400 focus:ring focus:ring-blue-300" type="date" name="dateofbirth" id="dateofbirth">
-             </div>
-         </div>
-         {{-- End Berlaku Hingga --}}
          {{-- Institusi --}}
              <div class="flex border-b border-solid border-gray-300">
                  <div class="px-4 py-3 text-sm w-60">
@@ -105,32 +66,11 @@
                  </div>
                  <div class="px-4 py-3 items-stretch flex-auto">
                      <div class="flex flex-auto items-stretch">
-                         <input type="text" id="small-input" class="flex-auto py-1 px-2 text-gray-900 border rounded text-sm border-solid border-gray-400 focus:ring focus:ring-blue-300">
+                         <input name="instituion" type="text" id="small-input" class="flex-auto py-1 px-2 text-gray-900 border rounded text-sm border-solid border-gray-400 focus:ring focus:ring-blue-300">
                      </div>
                  </div>
              </div>
          {{-- End Institusi --}}
-         {{-- Tipe Kanggotaan --}}
-         <div class="flex border-b border-solid border-gray-300">
-             <div class="px-4 py-3 text-sm w-60">
-                 <p class="font-bold text-sm">Tipe Kanggotaan</p>
-             </div>
-             <div class="px-4 py-3 text-sm">
-                 <p class="font-bold text-sm">:</p>
-             </div>
-             <div class="flex flex-auto items-stretch px-4 py-3">
-                 <div class="" style="width:200px;">
-                     <select class="w-52 min-w-fit text-black focus:ring focus:ring-blue-300 focus:border-blue-600 font-medium rounded border border-solid border-gray-400 text-sm px-2.5 py-1.5 mr-1 inline-flex items-center">
-                       <option value="0">Tipe Kanggotaan</option>
-                       <option value="1">Audi</option>
-                       <option value="2">BMW</option>
-                       <option value="3">Citroen</option>
-                       <option value="4">Ford</option>
-                     </select>
-                   </div>
-             </div>
-         </div>
-         {{-- End Tipe Kanggotaan --}}
          {{-- Jenis Kelamin --}}
          <div class="flex border-b border-solid border-gray-300">
              <div class="px-4 py-3 text-sm w-60">
@@ -141,9 +81,9 @@
              </div>
              <div class="flex text-sm px-4 py-3">
                  <div class="flex items-center justify-center">
-                     <input class="mr-2" type="radio" name="" id="">
+                     <input class="mr-2" type="radio" name="gender" value="Laki-laki" id="">
                      <label class="mr-3" for="">Laki-Laki</label>
-                     <input class="mr-2" type="radio" name="" id="">
+                     <input class="mr-2" type="radio" name="gender" value="Perempuan" id="">
                      <label for="">Perempuan</label>
                  </div>
              </div>
@@ -158,36 +98,10 @@
                  <p class="font-bold text-sm">:</p>
              </div>
              <div class="flex flex-auto items-stretch px-4 py-3">
-                 <textarea type="text"  class="flex-auto py-1 px-2 text-gray-900 border rounded text-sm border-solid border-gray-400 focus:ring focus:ring-blue-300" name="" id="" cols="30" rows="2"></textarea>
+                 <textarea type="text" name="address"  class="flex-auto py-1 px-2 text-gray-900 border rounded text-sm border-solid border-gray-400 focus:ring focus:ring-blue-300" name="" id="" cols="30" rows="2"></textarea>
              </div>
          </div>
          {{-- End Alamat --}}
-         {{-- Kode Pos --}}
-         <div class="flex border-b border-solid border-gray-300">
-             <div class="px-4 py-3 text-sm w-60">
-                 <p class="font-bold text-sm">Kode Pos</p>
-             </div>
-             <div class="px-4 py-3 text-sm">
-                 <p class="font-bold text-sm">:</p>
-             </div>
-             <div class="px-4 py-3">
-                 <input type="text" id="small-input" class="w-96 py-1 px-2 text-gray-900 border rounded text-sm border-solid border-gray-400 focus:ring focus:ring-blue-300">
-             </div>
-         </div>
-         {{-- End Kode Pos --}}
-         {{-- Alamat Surat --}}
-         <div class="flex border-b border-solid border-gray-300">
-             <div class="px-4 py-3 text-sm w-60">
-                 <p class="font-bold text-sm">Alamat Surat</p>
-             </div>
-             <div class="px-4 py-3 text-sm">
-                 <p class="font-bold text-sm">:</p>
-             </div>
-             <div class="flex flex-auto items-stretch px-4 py-3">
-                 <textarea type="text"  class="flex-auto py-1 px-2 text-gray-900 border rounded text-sm border-solid border-gray-400 focus:ring focus:ring-blue-300" name="" id="" cols="30" rows="2"></textarea>
-             </div>
-         </div>
-         {{-- End Alamat Surat --}}
          {{-- Nomor Telepon --}}
          <div class="flex border-b border-solid border-gray-300">
              <div class="px-4 py-3 text-sm w-60">
@@ -197,36 +111,10 @@
                  <p class="font-bold text-sm">:</p>
              </div>
              <div class="px-4 py-3">
-                 <input type="text" id="small-input" class="w-96 py-1 px-2 text-gray-900 border rounded text-sm border-solid border-gray-400 focus:ring focus:ring-blue-300">
+                 <input type="text" name="phone" id="small-input" class="w-96 py-1 px-2 text-gray-900 border rounded text-sm border-solid border-gray-400 focus:ring focus:ring-blue-300">
              </div>
          </div>
          {{-- End Nomor Telepon --}}
-         {{-- Nomor Identitas --}}
-         <div class="flex border-b border-solid border-gray-300">
-             <div class="px-4 py-3 text-sm w-60">
-                 <p class="font-bold text-sm">Nomor Identitas</p>
-             </div>
-             <div class="px-4 py-3 text-sm">
-                 <p class="font-bold text-sm">:</p>
-             </div>
-             <div class="px-4 py-3">
-                 <input type="text" id="small-input" class="w-96 py-1 px-2 text-gray-900 border rounded text-sm border-solid border-gray-400 focus:ring focus:ring-blue-300">
-             </div>
-         </div>
-         {{-- End Nomor Identitas --}}
-         {{-- Catatan --}}
-         <div class="flex border-b border-solid border-gray-300">
-             <div class="px-4 py-3 text-sm w-60">
-                 <p class="font-bold text-sm">Catatan</p>
-             </div>
-             <div class="px-4 py-3 text-sm">
-                 <p class="font-bold text-sm">:</p>
-             </div>
-             <div class="flex flex-auto items-stretch px-4 py-3">
-                 <textarea type="text"  class="flex-auto py-1 px-2 text-gray-900 border rounded text-sm border-solid border-gray-400 focus:ring focus:ring-blue-300" name="" id="" cols="30" rows="2"></textarea>
-             </div>
-         </div>
-         {{-- End Catatan --}}
          {{-- Foto --}}
          <div class="flex border-b border-solid border-gray-300">
              <div class="px-4 py-3 text-sm w-60">
@@ -240,7 +128,7 @@
                      <div class="bg-gray-300 rounded-md w-32 h-32"></div>
                      <div class="ml-3">
                          <label class="file">
-                             <input class=" border rounded text-sm" type="file" id="file" aria-label="File browser example">
+                             <input class=" border rounded text-sm" type="file" name="image" id="file" aria-label="File browser example">
                              <span class="file-custom"></span>
                            </label>
                      </div>
@@ -257,41 +145,16 @@
                  <p class="font-bold text-sm">:</p>
              </div>
              <div class="px-4 py-3">
-                 <input type="text" id="small-input" class="w-96 py-1 px-2 text-gray-900 border rounded text-sm border-solid border-gray-400 focus:ring focus:ring-blue-300">
+                 <input type="text" name="email" id="small-input" class="w-96 py-1 px-2 text-gray-900 border rounded text-sm border-solid border-gray-400 focus:ring focus:ring-blue-300">
              </div>
          </div>
          {{-- End Surel --}}
-         {{-- Kata sandi baru --}}
-         <div class="flex border-b border-solid border-gray-300">
-             <div class="px-4 py-3 text-sm w-60">
-                 <p class="font-bold text-sm">Kata sandi baru</p>
-             </div>
-             <div class="px-4 py-3 text-sm">
-                 <p class="font-bold text-sm">:</p>
-             </div>
-             <div class="px-4 py-3">
-                 <input type="text" id="small-input" class="w-96 py-1 px-2 text-gray-900 border rounded text-sm border-solid border-gray-400 focus:ring focus:ring-blue-300">
-             </div>
-         </div>
-         {{-- End Kata sandi baru --}}
-         {{-- Konfirmasi kata sandi baru --}}
-         <div class="flex border-b border-solid border-gray-300">
-             <div class="px-4 py-3 text-sm w-60">
-                 <p class="font-bold text-sm">Konfirmasi kata sandi baru</p>
-             </div>
-             <div class="px-4 py-3 text-sm">
-                 <p class="font-bold text-sm">:</p>
-             </div>
-             <div class="px-4 py-3">
-                 <input type="text" id="small-input" class="w-96 py-1 px-2 text-gray-900 border rounded text-sm border-solid border-gray-400 focus:ring focus:ring-blue-300">
-             </div>
-         </div>
-         {{-- End Konfirmasi kata sandi baru --}}
          {{-- Btn Simpan --}}
          <div class="flex border-b border-solid border-gray-300 px-4 py-3">
-             <button class="py-2 px-3 rounded text-white text-sm font-bold bg-blue-600 hover:bg-blue-500">Simpan</button>
-         </div>
-         {{-- End Btn Simpan --}}
+            <button type="submit"
+                class="py-2 px-3 rounded text-white text-sm font-bold bg-blue-600 hover:bg-blue-500">Simpan</button>
+        </div>
+        {{-- End Btn Simpan --}}
         </div>
     </form>
     {{-- End Section 2 --}}
