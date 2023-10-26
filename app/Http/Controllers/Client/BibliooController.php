5a747ed0358184747b8aa2deb4521cd25055c818
Andika Auli <dikaauli25@gmail.com>
2023-10-26 22:38:32 +0700

    [FIX] members

--- app/Http/Controllers/Client/BibliooController.php
+++ app/Http/Controllers/Client/BibliooController.php
@@ -14,7 +14,36 @@ class BibliooController extends Controller
      */
     public function index()
     {
-        //
+        $validator = Validator::make($request->all(), [
+            'name' => 'required|max:255|string',
+            'nim' => 'required|min:14|numeric', 'unique:member',
+            'gender' => 'required',
+            'birthdate' => 'required|date',
+            'address' => 'required|max:255',
+            'email' => 'required|max:255|email', 'unique:member',
+            'institution' => 'required|max:255|string',
+            'image' => 'required|image|max:2048|mimes:jpeg,png,jpg',
+            'phone' => 'required|min:11|numeric', 'unique:member',
+        ]);
+
+        $data = $request->all();
+        $imagePath = null;
+
+        if ($request->hasFile('image')) {
+            $image = $request->file('image');
+            $fileName = time() . '' . $image->getClientOriginalName();
+            $storeImage = $image->storeAs('public/images', $fileName);
+            $imagePath = asset(str_replace("public", "storage", $storeImage));
+            $data['image'] = $imagePath;
+            // $member->image = $fileName;
+        }
+
+        if ($validator->fails()) {
+            return response()->json($validator->errors(), 422);
+        }
+        $member = Member::create($data);
+        return response()
+            ->json(['message' => 'Anggota baru berhasil ditambahkan!', 'data' => $member]);
     }
 
     /**
