35cd57be1f26df5a86c4d07a59302ba5c72514f5
Andika Auli <dikaauli25@gmail.com>
2023-11-07 22:27:49 +0700

    [FIX] fetch edit data eksemplar

--- resources/views/dashboard/main.blade.php
+++ resources/views/dashboard/main.blade.php
@@ -12,8 +12,8 @@
 {{-- End Navbar --}}
 
     @vite('resources/js/app.js')
-    <script src="../path/to/flowbite/dist/datepicker.js"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/flowbite/1.8.1/datepicker.min.js"></script>
+    {{-- <script src="../path/to/flowbite/dist/datepicker.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/flowbite/1.8.1/datepicker.min.js"></script> --}}
     </div>
     </body>
 </html>
