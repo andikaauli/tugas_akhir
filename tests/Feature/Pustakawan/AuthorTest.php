f7f449c695894ee553d481ddf9f5b762c604d12c
Abimanyupy <abndung@gmail.com>
2023-11-16 06:28:43 +0700

    testing testing

---
+++ tests/Feature/Pustakawan/AuthorTest.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace Tests\Feature\Pustakawan;
+
+use Tests\TestCase;
+// use App\Models\Author;
+use Illuminate\Foundation\Testing\WithFaker;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+
+class AuthorTest extends TestCase
+{
+    /**
+     * A basic feature test example.
+     *
+     * @return void
+     */
+
+    // public function test_pustakawan_dapat_melihat_author()
+    // {
+    //     $response = $this->get('/api/author');
+    //     $response->assertStatus(200);
+    //     // $response->assertSee('voluptas');
+    // }
+
+    // public function test_pustakawan_can_add_new_author()
+    // {
+    //     $response = $this->post('/api/author/add', [
+    //         'title' => 'Filon',
+    //         'born_date' => '2000',
+    //     ]);
+
+    //     $response->assertStatus(200);
+    // }
+
+    // public function test_pustakawan_can_edit_author()
+    // {
+    //     $response = $this->post('/api/author/edit/12', [
+    //         'title' => 'Filosssssssn',
+    //         'born_date' => '2001',
+    //     ]);
+    //     $response->assertStatus(200);
+    // }
+
+    // public function test_pustakawan_can_delete_author()
+    // {
+    //     $response = $this->delete('/api/author/destroy/18');
+    //     $response->assertStatus(200);
+    // }
+}
